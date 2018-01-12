297e76477ca1c42c283bf6dfacaf7ea5ec1994bd
Ben Keith <ben@benlk.com>
2018-01-12 17:14:36 -0500

    remove SEEN roundups widget

--- wp-content/themes/midwestenergynews/inc/widgets.php
+++ wp-content/themes/midwestenergynews/inc/widgets.php
@@ -4,7 +4,6 @@ function mwen_widgets() {
 	$register = array(
 		'mwen_mailchimp_signup_widget'	=> '/inc/widgets/mwen-mailchimp.php',
 		'mwen_roundups_widget'	=> '/inc/widgets/mwen-roundups.php',
-		'seen_roundups_widget'	=> '/inc/widgets/seen-link-roundups-widget.php'
 	);
 	foreach ( $register as $key => $val ) {
 		require_once( get_stylesheet_directory() . $val );
