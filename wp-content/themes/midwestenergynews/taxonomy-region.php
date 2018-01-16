1446d5408dc8df49214ff22d0e209b51bae15bf5
Julia Smith <julia@inn.org>
2018-01-16 08:34:52 -0600

    fix for stray comment

--- wp-content/themes/midwestenergynews/taxonomy-region.php
+++ wp-content/themes/midwestenergynews/taxonomy-region.php
@@ -181,24 +181,6 @@ $queried_object = get_queried_object();
 			</div>
 		</nav> -->
 
-		<!--<div class="row-fluid clearfix">
-			<div class="stories span8" role="main" id="content">
-			<?php/*
-				// and finally wind the posts back so we can go through the loop as usual
-				rewind_posts();
-				$counter = 1;
-				while ( have_posts() ) : the_post();
-					$post_type = get_post_type();
-					$partial = largo_get_partial_by_post_type( 'archive', $post_type, 'archive' );
-					get_template_part( 'partials/content', $partial );
-					do_action( 'largo_loop_after_post_x', $counter, $context = 'archive' );
-					$counter++;
-				endwhile;
-
-				largo_content_nav( 'nav-below' );*/
-			?>
-			</div>
-			<?php get_sidebar(); ?>-->
 		</div>
 		<?php } else {
 			get_template_part( 'partials/content', 'not-found' );
