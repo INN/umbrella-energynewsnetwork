cb9b46a3214905af033e7e11f87e31024d20d2e7
Ben Keith <ben@benlk.com>
2018-01-16 10:21:21 -0500

    Create ze files for the Energy News Link Roundups widget

--- wp-content/themes/midwestenergynews/inc/widgets.php
+++ wp-content/themes/midwestenergynews/inc/widgets.php
@@ -5,6 +5,12 @@ function mwen_widgets() {
 		'mwen_mailchimp_signup_widget'	=> '/inc/widgets/mwen-mailchimp.php',
 		'mwen_roundups_widget'	=> '/inc/widgets/mwen-roundups.php',
 	);
+
+	// this is meaningless if the USEN Regions taxonomy is not available
+	if ( class_exists( 'USEN_Regions_Taxonomy' ) && taxonomy_exists( 'region' ) ) {
+		$includes['usen_roundups_widget'] = '/inc/widgets/usen-roundups.php';
+	}
+
 	foreach ( $register as $key => $val ) {
 		require_once( get_stylesheet_directory() . $val );
 		register_widget( $key );
