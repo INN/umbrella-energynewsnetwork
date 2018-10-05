0ab5a661c4e51660d4f73c76f38df8eea30168f6
Ben Keith <ben@benlk.com>
2018-10-05 14:18:48 -0400

    Add page title back to the subscription template

--- wp-content/themes/midwestenergynews/partials/content-subscription-page.php
+++ wp-content/themes/midwestenergynews/partials/content-subscription-page.php
@@ -5,6 +5,14 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?>>
+	<header>
+		<h1 class="entry-title" itemprop="headline"><?php the_title(); ?></h1>
+		<?php
+			if ( current_user_can( 'edit_post', get_the_id() ) ) {
+				echo ' <span class="edit-link"><a href="' . get_edit_post_link( get_the_ID() ) . '">' . __( 'Edit This Post', 'largo' ) . '</a></span>';
+			}
+		?>
+	</header>
 	<section class="entry-content">
 		<?php do_action('largo_before_page_content'); ?>
 		<?php the_content(); ?>
