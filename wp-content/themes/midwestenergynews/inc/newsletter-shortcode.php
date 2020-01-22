d01a623ca294ead22a0a83bf4599c2ba810047d4
Ben Keith <ben@benlk.com>
2020-01-22 16:04:03 -0500

    remove automatic addition of newsletter to front of old stories.

--- wp-content/themes/midwestenergynews/inc/newsletter-shortcode.php
+++ wp-content/themes/midwestenergynews/inc/newsletter-shortcode.php
@@ -16,14 +16,3 @@ function mwen_newsletter_signup($attrs=null) {
 	return ob_get_clean();
 }
 add_shortcode( 'newsletter_signup', 'mwen_newsletter_signup' );
-
-/**
- * Also render the newsletter signup form on pages published before June 30, 2015
- */
-function mwen_add_newsletter_signup_to_old_posts() {
-	global $post;
-	if ( !is_admin() && strtotime( '2015-06-30 00:00:00' ) > strtotime( $post->post_date ) ) {
-		print mwen_newsletter_signup( array( 'reverse' => true ) );
-	}
-}
-add_action( 'mwen_pre_largo_entry_content', 'mwen_add_newsletter_signup_to_old_posts' );
