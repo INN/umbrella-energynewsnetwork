d08855327accc58a9b3f691bd4956df4e0b7b07f
Ben Keith <ben@benlk.com>
2020-02-07 15:08:03 -0500

    Persist checkedness across page loads, in response to query params

--- wp-content/themes/midwestenergynews/partials/digest-search.php
+++ wp-content/themes/midwestenergynews/partials/digest-search.php
@@ -10,7 +10,7 @@
 		<input type="text" placeholder="<?php _e('Search', 'largo'); ?>" class="searchbox search-query" value="<?php the_search_query(); ?>" name="digest-search" /><button type="submit" name="search submit" class="search-submit btn"><?php _e('Go', 'largo'); ?></button>
 	</div>
 
-	<fieldset id="filter-buttons">
+	<fieldset id="filter-buttons" name="region-selectors">
 		<legend>Select a region:</legend>
 		<?php
 			$regions = get_terms(
@@ -19,12 +19,28 @@
 					'hide_empty' => 'true',
 				),
 			);
+
+			// find the submitted query
+			$region_query_params = ( isset( $_GET['digest-search-region'] ) ) ? $_GET['digest-search-region'] : array() ;
+			if ( is_string( $region_query_params ) ) {
+				$region_query_params = array( $region_query_params );
+			}
+			$region_query_params = array_map( 'sanitize_title_for_query', $region_query_params );
+			error_log(var_export( $region_query_params, true));
+
 			foreach ( $regions as $region ) {
+				$checked = '';
+				// doing this because shoehorning in_array() into checked() would be unreadable
+				if ( in_array( $region->slug, $region_query_params ) ) {
+					$checked = 'checked="checked"';
+				}
+
 				printf(
-					'<label class="checkbox"><input name="region" type="checkbox" class="term-%1$s btn" value="%2$s">%3$s</label>',
+					'<label class="checkbox"><input name="digest-search-region[]" type="checkbox" class="term-%1$s btn" value="%2$s" %4$s>%3$s</label>',
 					esc_attr( $region->term_id ),
 					esc_attr( $region->slug ),
-					esc_html( $region->name )
+					esc_html( $region->name ),
+					$checked
 				);
 			}
 
